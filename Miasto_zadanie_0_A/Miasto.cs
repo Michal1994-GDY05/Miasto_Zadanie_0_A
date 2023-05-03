65e2867c9918acc05fc13fc13809f61248151193
Michal Jakusz <jakusz.it@gmail.com>
2023-05-03 20:16:26 +0200

    Push

--- Miasto_zadanie_0_A/Miasto.cs
+++ Miasto_zadanie_0_A/Miasto.cs
@@ -4,9 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Miasto_Zadanie_0_A.Miasto_zadanie_0_A
+namespace Miasto_Zadanie_0_A
 {
-    internal class Miasto
+    public class Miasto
     {
+        public string Nazwa { get; set; }
+        public uint Populacja { get; set; }
+        public double Powierzchnia
+
+        {
+            get { return powierzchnia; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Powierzchnia musi być większa od 0.");
+                }
+                powierzchnia = value;
+            }
+        }
+
+        private double powierzchnia;
+        public double GestoscZaludnienia
+        {
+            get { return Math.Round(Populacja / Powierzchnia, 2); }
+        }
+
+        public Miasto(string nazwa, uint populacja, double powierzchnia)
+        {
+            Nazwa = nazwa;
+            Populacja = populacja;
+            Powierzchnia = powierzchnia;
+        }
     }
 }
