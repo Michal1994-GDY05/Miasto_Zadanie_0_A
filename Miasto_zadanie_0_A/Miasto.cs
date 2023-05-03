a0baf8ccb51412221e44448671c3ea902f19fe3b
Michal Jakusz <jakusz.it@gmail.com>
2023-05-03 20:12:51 +0200

    Dodaj pliki projektów.

---
+++ Miasto_zadanie_0_A/Miasto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miasto_Zadanie_0_A.Miasto_zadanie_0_A
+{
+    internal class Miasto
+    {
+    }
+}
---
+++ Miasto_zadanie_0_A/Miasto_program.cs
@@ -0,0 +1,17 @@
+﻿internal class Miasto_program
+{
+    static void Main(string[] args)
+    {
+        List<Miasto> miasta = new List<Miasto>
+    {
+        new Miasto("Gdynia", 243770, 130.79),
+        new Miasto("Gdańsk", 486022, 263.44),
+        new Miasto("Sopot", 32474, 17.28)
+    };
+
+        foreach (Miasto miasto in miasta)
+        {
+            Console.WriteLine("Gęstość zaludnienia miasta {0} wynosi {1} os/km2", miasto.Nazwa, miasto.GestoscZaludnienia);
+        }
+    }
+}
